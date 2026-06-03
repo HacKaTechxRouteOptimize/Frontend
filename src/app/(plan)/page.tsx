6564eec3e5b7a0e1f160b6eb345dc1693cbb1f03
Pattararnxx <pattaraporn.po@kkumail.com>
2026-06-03 12:14:07 +0700

    fix alert

--- src/app/(plan)/page.tsx
+++ src/app/(plan)/page.tsx
@@ -225,6 +225,7 @@ const Preview = () => {
   const [colDataOrder, setColDataOrder] = useState<string[][]>([]);
   const [isOptimize, setIsOptimize] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
+  const [locationError, setLocationError] = useState("");
   const getFileCondition = (): string => {
     if (vehicleBases.length === 0 && orderBases.length === 0) {
       return "ยังไม่ได้อัปโหลดไฟล์ กรุณาอัปโหลดข้อมูลรถและออเดอร์ให้ครบถ้วน";
@@ -290,8 +291,10 @@ const Preview = () => {
   };
 
   const handleUseCurrentLocation = () => {
+    setLocationError("");
+
     if (!navigator.geolocation) {
-      alert("เบราว์เซอร์ไม่รองรับการระบุตำแหน่ง");
+      setLocationError("*เบราว์เซอร์ไม่รองรับการระบุตำแหน่ง");
       return;
     }
 
@@ -301,22 +304,25 @@ const Preview = () => {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         });
+
+        setLocationError("");
       },
       (error) => {
-        console.error(error);
-
         switch (error.code) {
           case error.PERMISSION_DENIED:
-            alert("กรุณาอนุญาตการเข้าถึงตำแหน่ง");
+            setLocationError("*กรุณาอนุญาตการเข้าถึงตำแหน่ง");
             break;
+
           case error.POSITION_UNAVAILABLE:
-            alert("ไม่สามารถระบุตำแหน่งได้");
+            setLocationError("*ไม่สามารถระบุตำแหน่งได้");
             break;
+
           case error.TIMEOUT:
-            alert("หมดเวลาการค้นหาตำแหน่ง");
+            setLocationError("*หมดเวลาการค้นหาตำแหน่ง");
             break;
+
           default:
-            alert("เกิดข้อผิดพลาด");
+            setLocationError("*เกิดข้อผิดพลาด");
         }
       },
       {
@@ -662,6 +668,10 @@ const Preview = () => {
                 ใช้ตำแหน่งปัจจุบัน
               </button>
             </div>
+
+            {locationError && (
+              <p className={styles.locationError}>{locationError}</p>
+            )}
             <section className={styles.uploadContainer}>
               <div
                 style={{
